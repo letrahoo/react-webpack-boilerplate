4cae30dc7be8d5a5d359c770c6c0ae0914d13072
letra <letra@bingyan.net>
2018-10-30 16:51:53 +0800

    :heavy_plus_sign: feat: add webpack plugins and tools for config dev/prod

--- config/config.js
+++ config/config.js
@@ -1,11 +1,21 @@
 module.exports = {
   common: {
-    
+    htmlConfig: {
+      title: 'App',
+      template: 'index.html',
+      filename: 'index.html',
+      favicon: undefined,
+    },
   },
   dev: {
-    
+    publicPath: '/',
+    port: 3000,
+    devtool: 'inline-source-map',
+    proxy: {},
   },
   prod: {
-
-  }
+    publicPath: '/',
+    devtool: 'source-map',
+    outputPath: 'dist',
+  },
 }
--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,10 +1,65 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
 const common = require('./webpack.common');
+const config = require('./config');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map',
+  output: {
+    filename: '[name].bundle.js',
+    publicPath: config.dev.publicPath,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      ...config.common.htmlConfig,
+    }),
+    new StyleLintPlugin({
+      emitErrors: false
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        use: {
+          loader: 'eslint-loader',
+          options: {
+            emitWarning: true,
+          },
+        },
+        exclude: /node_modules/
+      }, {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      }, {
+        test: /.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              module: true,
+              localIdentName: '[name]__[local]--[hash:base64:5]',
+            },
+          },
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  devtool: config.dev.devtool,
   devServer: {
-    contentBase: './dist',
+    contentBase: false,
+    hot: true,
+    port: config.dev.port,
+    proxy: config.dev.proxy,
   },
 });
