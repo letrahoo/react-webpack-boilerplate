4da0cb11aa992506a5f090cba77bf25e6a5f0d3e
Letra312 <hxq15827562797@gmail.com>
2018-10-30 17:14:44 +0800

    :bug: fix: fix some word bugs
    fix bug  eg. plugin(s), extension...

---
+++ src/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import App from './App';
+
+const root = document.createElement('div');
+document.body.appendChild(root);
+
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    root
+  );
+};
+
+render(App);
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    /* eslint-disable */
+    const nextApp = require('./App');
+    /* eslint-enable */
+    render(nextApp.default);
+  });
+}
