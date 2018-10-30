4a8b3932008f2fc35e5a2cba7f0e9b7a2e5d4cf9
Letra312 <hxq15827562797@gmail.com>
2018-10-30 11:44:54 +0800

    :art: feat: add dependencies of developing and production

---
+++ config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  common: {
+    
+  },
+  dev: {
+    
+  },
+  prod: {
+
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
   entry: {
     app: './src/index.js',
-    print: './src/print.js',
   },
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: '123123'
     }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   // module: {
   //   rules: [
